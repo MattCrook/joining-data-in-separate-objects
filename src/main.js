068f43c51be8714b1df92a36290680be0bd01dec
Matt Crook <matt.crook11@gmail.com>
2020-02-13 09:15:39 -0600

    WIP comit

--- src/main.js
+++ src/main.js
@@ -1,71 +1,46 @@
 const apiManager = {
-  async getEmployee() {
+  async getEmployees() {
     const employeeResponse = await fetch("http://localhost:8088/employees");
     return await employeeResponse.json();
   },
-  async getDepartment() {
+  async getDepartments() {
     const departmentResponse = await fetch("http://localhost:8088/departments");
     return await departmentResponse.json();
   },
-  async getComputer() {
+  async getComputers() {
     const computerResponse = await fetch("http://localhost:8088/computers");
     return await computerResponse.json();
   }
 };
-// let employeesArr = [];
-// let depArr = [];
-// let compArr = [];
 
-let entityArray = [];
 // returning an array of arrays
 const getEntity = async () => {
-  const name = apiManager.getEmployee();
-  const department = apiManager.getDepartment();
-  const computers = apiManager.getComputer();
-  let values = await Promise.all([name, department, computers]);
-  values.map(entities => {
-    // entities is array of objects
-    return entityArray.push(entities);
-  });
-  console.log("array:", entityArray);
-  entityArray.forEach(resource => {
-    // resource is those objects extracted
-    const name = resource[0].name;
-    console.log(name)
-    const department = resource[1].department_name;
-    console.log(department)
-    const computers = resource[2].computer_name;
-    console.log(computers)
-    //Object.entries(resource).forEach(foreignKey => {
-      // extracting key alue pairs to test for strings (the foreign keys)
-      //if (typeof foreignKey == "string") {
-        const employeeObject = { name, department, computers };
-        const employeeHTML = htmlSkeleton(employeeObject);
-        renderEmployee(employeeHTML);
-     // }
-    //});
+  const employeeName = apiManager.getEmployees;
+  const employeeDepartment = apiManager.getDepartments;
+  const employeeComputers = apiManager.getComputers;
+  let values = await Promise.all([
+    employeeName(),
+    employeeDepartment(),
+    employeeComputers()
+  ]);
+
+  const returnedEmployeeObject = values[0];
+  const returnedDepartmentObject = values[1];
+  const returnedComputersObject = values[2];
+  returnedEmployeeObject.forEach(employee => {
+    const departmentObject = returnedDepartmentObject.filter(department => department.id === employee.departmentId
+    );
+    const computerObject = returnedComputersObject.filter(computer => computer.id === employee.computerId
+    );
+    const name = employee.name;
+    const department = departmentObject.department_name;
+    console.log(departmentObject);
+    const computer = computerObject.computer_name;
+    const employeeObject = { name, department, computer };
+    const employeeHTML = htmlSkeleton(employeeObject);
+    renderEmployee(employeeHTML);
   });
 };
-
-// const filterEntity = async returnedPromiseArray => {
-//   for (let entity of Object.keys(returnedPromiseArray)) {
-//     const filterResource = returnedPromiseArray[entity];
-//     return await filterResource;
-//   }
-// };
-
-// const renderToDom = async getEntity => {
-//   getEntity.map(async element => {
-//     element.forEach(employee => {
-//       const name = employee.name;
-//       const department = employee.department_name;
-//       const computers = employee.computer_model;
-//       const employeeObject = { name, department, computers };
-//       const employeeHTML = htmlSkeleton(employeeObject);
-//       renderEmployee(employeeHTML);
-//     });
-//   });
-// };
 
 const htmlSkeleton = ({ name, department, computer }) => {
   return `
@@ -89,3 +64,15 @@ const renderEmployee = employeeHTML => {
 };
 
 getEntity().catch(err => console.log(err));
+
+// const which hold value of dept names array of objeect names ect... 3 of them
+// for each eployee as  ieteating thru those get ref to naem and inside tht for loop and do filter for that item dept id ad filter on dept name and same for employee object
+// and once ref to both of those
+// once finish filter back oin scope of employee object
+
+// one for each and inside of that scope are 2 sep filters
+// return what you are filtering on one liner or have to use return
+// do that for each eployee object
+// once you have ref to neam dept, computer...all within the foreach.
+
+// only forEach inside of employees
